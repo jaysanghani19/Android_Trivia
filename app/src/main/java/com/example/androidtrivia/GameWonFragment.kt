6cba99aaa0cf8751d47631a9dc65962d9a03baaf
jaysanghani19 <jaysanghani0@gmail.com>
2023-03-06 12:51:09 +0530

    Adding Animation

--- app/src/main/java/com/example/androidtrivia/GameWonFragment.kt
+++ app/src/main/java/com/example/androidtrivia/GameWonFragment.kt
@@ -3,7 +3,6 @@ package com.example.androidtrivia
 import android.content.Intent
 import android.os.Bundle
 import android.view.*
-import android.widget.Toast
 import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -50,8 +49,7 @@ class GameWonFragment : Fragment() {
     private fun shareIntent() {
         try {
             startActivity(getShareIntent())
-        }
-        catch (ex : android.content.ActivityNotFoundException){
+        } catch (ex: android.content.ActivityNotFoundException) {
 
         }
     }
